1e2fc6c34cd2d2bdcd7930f01b204dcd2f17896f
arturgspb <art@realweb.ru>
2016-08-03 16:52:40 +0300

    [~] Исправление проблем с выводом логов в stdout

--- metaappscriptsdk/logger/__init__.py
+++ metaappscriptsdk/logger/__init__.py
@@ -2,6 +2,7 @@
 
 import logging
 
+import sys
 from fluent import handler
 
 from metaappscriptsdk.logger.custom_fluent import FluentHandler
@@ -16,8 +17,12 @@ def create_logger(service_id=None, debug=True):
     if not service_id:
         service_id = 'unknown'
     # http://stackoverflow.com/questions/3220284/how-to-customize-the-time-format-for-python-logging
-    logging.basicConfig(format='%(asctime)s:%(levelname)s: %(message)s', datefmt="%H:%M:%S",
-                        level=logging.INFO)
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.INFO)
+    ch = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter('%(asctime)s:%(levelname)s: %(message)s',  datefmt="%H:%M:%S")
+    ch.setFormatter(formatter)
+    root_logger.addHandler(ch)
 
     if not debug:
         custom_format = {
@@ -31,5 +36,4 @@ def create_logger(service_id=None, debug=True):
         h = FluentHandler('es.metaappscript', host='code.harpoon.lan', port=8899, verbose=False)
         formatter = handler.FluentRecordFormatter(custom_format)
         h.setFormatter(formatter)
-        root_logger = logging.getLogger()
         root_logger.addHandler(h)
