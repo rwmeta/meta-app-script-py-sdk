8f12691e039dd1df029718355e9132604334236e
arturgspb <art@realweb.ru>
2016-08-19 20:00:37 +0300

    [+] API для чтения задач запускатора из потока

---
+++ metaappscriptsdk/examples/test_tasks.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+import time
+
+from metaappscriptsdk import MetaApp
+
+META = MetaApp()
+log = META.log
+
+# Установка таской для отладки кода
+META.worker.debug_tasks = [{
+    "data": {
+        "username": "Artur"
+    }
+}]
+
+
+@META.worker.single_task
+def main(task):
+    log.info('Hello task')
+    time.sleep(10)
+
+    data = task['data']
+    username_ = data.get('username', '%USERNAME%')
+    log.info('Hello task', {'welcome': 'Hello, ' + username_, 't': task})
+    time.sleep(10)
