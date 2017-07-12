688bf246d3c31ce496728dbe1c5130f11c5acc58
arturgspb <art@realweb.ru>
2017-07-12 13:04:41 +0300

    [+] ExportApi.export_data_source для экспорта данных фида

--- metaappscriptsdk/examples/export_page.py
+++ metaappscriptsdk/examples/export_page.py
@@ -4,5 +4,8 @@ META = MetaApp(meta_url="http://localhost:8080")
 log = META.log
 
 export = META.ExportService
-res = export.export_page(58, 3203, ["res"], export_format="html")
-print(u"res = %s" % str(res))
+# res = export.export_page(58, 3203, ["res"], export_format="html")
+# print(u"res = %s" % str(res))
+
+res_ds = export.export_data_source('e4082545-7388-4e2c-893f-3c0124fc5d26')
+print(u"res_ds = %s" % str(len(res_ds['rows'])))
