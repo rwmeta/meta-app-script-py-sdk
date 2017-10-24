4be5549a34b76f490dba6f592fb24687e6b2f555
arturgspb <arturgspb@gmail.com>
2017-10-24 23:29:07 +0300

    Фикс отлова ошибок соединения с метов

--- metaappscriptsdk/__init__.py
+++ metaappscriptsdk/__init__.py
@@ -188,7 +188,7 @@ class MetaApp(object):
             "timeout": (60, 1800)
         }
 
-        for try_idx in range(6):
+        for try_idx in range(8):
             try:
                 # Пока непонятно почему частенько получаем ошибку:
                 # Failed to establish a new connection: [Errno 110] Connection timed out',))
@@ -206,7 +206,7 @@ class MetaApp(object):
                     raise UnexpectedResponseError()
                 else:
                     process_meta_api_error_code(resp.status_code, request, resp.text)
-            except (ConnectionError, TimeoutError) as e:
+            except (requests.exceptions.ConnectionError, ConnectionError, TimeoutError) as e:
                 self.log.warning('META API Connection Error. Sleep...', {"e": e})
                 time.sleep(15)
 
@@ -247,7 +247,7 @@ class MetaApp(object):
             request['data'] = json.dumps(data)
         request['headers'] = _headers
 
-        for try_idx in range(6):
+        for try_idx in range(8):
             try:
                 # Пока непонятно почему частенько получаем ошибку:
                 # Failed to establish a new connection: [Errno 110] Connection timed out',))
@@ -266,7 +266,7 @@ class MetaApp(object):
                     # raise UnexpectedResponseError()
                 else:
                     process_meta_api_error_code(resp.status_code, request, resp.text)
-            except (ConnectionError, TimeoutError) as e:
+            except (requests.exceptions.ConnectionError, ConnectionError, TimeoutError) as e:
                 self.log.warning('META API Connection Error. Sleep...', {"e": e})
                 time.sleep(15)
 
