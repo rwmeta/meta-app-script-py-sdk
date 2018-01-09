2398e44c8afe97b00326714802248ad3a6629765
arturgspb <art@realweb.ru>
2018-01-09 19:18:08 +0300

    [~] Исправление проблемы, которая приводила к непередаче appAlias в UsersManagementApi.send_recovery_notice

--- metaappscriptsdk/services/UserManagementService.py
+++ metaappscriptsdk/services/UserManagementService.py
@@ -17,7 +17,7 @@ class UserManagementService:
         """
         data = {
             "login": login,
-            "app_alias": app_alias,
+            "appAlias": app_alias,
             "state": state,
         }
         response = self.__app.native_api_call('user-management', 'sendRecoveryNotice', data, self.__options, False, None, False, http_path="/api/meta/v1/", http_method="POST")
