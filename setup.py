c7ec601aaf504b3d4f776b9ef5fb67ec18d58a2e
arturgspb <art@realweb.ru>
2018-06-18 15:23:09 +0300

    [~] setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from os import path
 
 import setuptools
 
-try:  # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError:  # for pip <= 9.0.3
-    from pip.req import parse_requirements
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
 
 from metaappscriptsdk import info
 
@@ -25,11 +26,8 @@ packages = [
     'metaappscriptsdk.starter',
 ]
 
-install_reqs = parse_requirements(path.join(here, 'requirements.txt'), session=False)
-
-# reqs is a list of requirement
-# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
-reqs = [str(ir.req) for ir in install_reqs]
+install_reqs = parse_requirements('requirements.txt')
+reqs = install_reqs
 
 setuptools.setup(
     name=info.__package_name__,
