09c7fc05d3689a9db73d9292625111a5c2775b55
Александр Степанов <a.stepanov@realweb.ru>
2018-06-27 18:18:50 +0300

    MASTER [~] Ошибка в запросе

--- metaappscriptsdk/examples/feed_api/fetch_datasource.py
+++ metaappscriptsdk/examples/feed_api/fetch_datasource.py
@@ -3,10 +3,11 @@ from metaappscriptsdk import MetaApp
 META = MetaApp(meta_url="http://localhost:8080")
 
 FeedSrv = META.FeedService
-data_result = FeedSrv.datasource_fetch("c99e4390-99fa-4bc5-b1d0-4d6fed06893d")
+# deprecated
+# data_result = FeedSrv.datasource_fetch("c99e4390-99fa-4bc5-b1d0-4d6fed06893d")
 
-for row in data_result['rows']:
-    print(u"name = %s" % str(row['name']))
+# for row in data_result['rows']:
+#     print(u"name = %s" % str(row['name']))
 
 # Result
 # name = Юрий Гагарин
